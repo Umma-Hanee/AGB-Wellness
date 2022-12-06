edc30b22fe6b01cd2c57cda59b0b8f828d459d50
Umma-Hanee <79915079+Umma-Hanee@users.noreply.github.com>
2022-12-06 23:55:39 +0400

    done

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()// loads data from .env file
-
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 const router = require('./routes/agbwellnessRoutes');
 const path = require('path');
 const public = path.join(__dirname, 'public');
 const mustache = require('mustache-express');
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
+
+
 
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
@@ -16,7 +17,6 @@ app.use(express.static(public));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/',router);
-
 app.listen(3000, () => {
     console.log('Server started on port 3000. Ctrl^c to quit.');
 })
